97bd7093aec62543771329fc81745f08914e2df4
Frankovsk1y <you@example.comvladfrankovskiy@mail.ru>
2023-01-31 20:54:05 +0300

    add new files and fix bugs

---
+++ JS/script.js
@@ -0,0 +1,5 @@
+const designContact=document.querySelector(".design__button")
+const buttonContact=document.querySelector(".button__form")
+designContact.onclick=function(){
+    buttonContact.classList.toggle("active")
+}
